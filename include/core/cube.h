e22ee0cfb3cc5fa443d006f936773abb804e59a0
Kajrakso <ofjakob@gmail.com>
2024-12-23 21:02:02 +0100

    improved comment.

--- include/core/cube.h
+++ include/core/cube.h
@@ -21,7 +21,8 @@ cube_t cube_create_new_cube();
 bool cube_operation_is_equal(cube_t cube1, cube_t cube2);
 
 /* This function returns the composition of cube2 and cube1 in the Rubik's cube group.
-That is, first apply cube1, then cube2. */
+That is, first apply cube1, then cube2.
+`cube_move_apply_move` should be used when applying moves to the cube.*/
 cube_t cube_operation_compose(cube_t cube1, cube_t cube2);
 
 /* This function returns the inverse of the cube. That is, the cube C^(-1) that satisfies C^(-1)*C = I. */
