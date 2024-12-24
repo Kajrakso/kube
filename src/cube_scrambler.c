47e2d3e8df4a7a5ea883badca5ca35a88609d12d
Kajrakso <ofjakob@gmail.com>
2024-12-24 11:53:31 +0100

    bad scrambler is less bad

--- src/cube_scrambler.c
+++ src/cube_scrambler.c
@@ -7,13 +7,7 @@ cube_t cube_scrambler_get_scrambled_cube(){
 
     // https://arxiv.org/pdf/2410.20630
     const int n_rand_moves = 26;
-
-    int moves[n_rand_moves];
     for (int i = 0; i < n_rand_moves; i++){
-        moves[i] = rand() % NMOVES;
-    }
-
-    for (int i = 0; i < 26; i++){
-        cube_apply_move(&cube, moves[i]);
+        cube_apply_move(&cube, rand() % NMOVES);
     }
 }
