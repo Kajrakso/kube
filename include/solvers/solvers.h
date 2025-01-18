2eef6931ccb55ff68c2e9030696da5a29807e540
Kajrakso <ofjakob@gmail.com>
2025-01-18 18:07:32 +0100

    solvers: implemented IDDFS Implemented IDDFS as a first step towards a solver.

--- include/solvers/solvers.h
+++ include/solvers/solvers.h
@@ -8,8 +8,5 @@
 /* Solve a cube. Returns an array of moves */
 int* cube_solvers_solve_cube(cube_t* cube, size_t* solution_length);
 
-/* Finds the length of the shortest solution using BFS.
-OBS: This is the naive approch and is very slow. */
-size_t cube_solvers_solve_cube_BFS(cube_t* cube, int* moveset, size_t moveset_length);
 
 #endif // _SOLVERS_H_
