0e1f37be50bacc1f6839b962cfaaa831751bdb2c
Kajrakso <ofjakob@gmail.com>
2025-05-09 00:23:11 +0200

    forgot to update the test

--- tests/cube_cclass_tests.c
+++ tests/cube_cclass_tests.c
@@ -19,14 +19,6 @@ Test(cclass, cclass_invariant_under_UDsyms){
   cube_tables_generate();
   precompute_combinatorials();
 
-  const uint64_t size_cclass =
-    sizeof(struct c_index_cclass_sym) * NCCU * NCO;
-
-  struct c_index_cclass_sym* cclass = malloc(size_cclass);
-
-  fprintf(stderr, "Loading cclass table\n");
-  load_cclasstable("data/cclass.dat", cclass, size_cclass);
-
   static int UDsyms[] = {
     0, 1, 2, 3, 16, 17, 18, 19,     // no inversion
     24, 25, 26, 27, 40, 41, 42, 43  // inversion
@@ -37,21 +29,20 @@ Test(cclass, cclass_invariant_under_UDsyms){
   for (int k = 0; k < 100; k++){
     cube_t cube = cube_scrambler_get_handscrambled_cube();
 
-    uint32_t ci = cclass[cube_to_c_index(&cube)].cclass_i;
-    uint32_t c = cclass[cube_to_c_index(&cube)].cclass;
+    uint32_t ci = cclass_table[cube_to_c_index(&cube)].cclass_i;
+    uint32_t c = cclass_table[cube_to_c_index(&cube)].cclass;
     uint32_t ci2, c2;
 
     for (int i = 1; i < 16; i++){
       int t = UDsyms[i];
       cube_t cube2 = cube_operation_sym_conjugate(cube, t);
 
-      ci2 = cclass[cube_to_c_index(&cube2)].cclass_i;
-      c2 = cclass[cube_to_c_index(&cube2)].cclass;
+      ci2 = cclass_table[cube_to_c_index(&cube2)].cclass_i;
+      c2 = cclass_table[cube_to_c_index(&cube2)].cclass;
 
       cr_assert_eq(ci, ci2);
       cr_assert_eq(c2, c2);
     }
   }
 
-  free(cclass);
 }
